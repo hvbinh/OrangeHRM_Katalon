014b98818ef7b45971a96ed40ff93520bee27b8d
hvbinh <havanbinhk13t01@gmail.com>
2022-05-06 14:59:49 +0700

    add new TC testing

---
+++ Object Repository/Leave/Assign_Leave_Title.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Assign_Leave_Title</name>
+   <tag></tag>
+   <elementGuidId>4ff8d8f2-6f4c-4264-826b-1e002b48b7c9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//h1[text()='Assign Leave']</value>
+      </entry>
+      <entry>
+         <key>IMAGE</key>
+      </entry>
+      <entry>
+         <key>CSS</key>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
